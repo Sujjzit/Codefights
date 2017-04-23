e8e1264b5314eec1435f2a55f3b2ea03ebc9748e
Sujjzit <sujjzit91@gmail.com>
2017-04-23 14:30:43 -0400

    Create alphabeticShift.py

---
+++ alphabeticShift.py
@@ -0,0 +1,14 @@
+"""
+"""
+
+def alphabeticShift(inputString):
+    char=list(inputString)
+    for i in range(len(char)):
+        if char[i]=='z':
+            char[i]='a'
+        else:    
+            number=ord(char[i])-ord('a')
+            number+=1
+            char[i]=chr(number+ord('a'))
+        
+    return ''.join(char)
